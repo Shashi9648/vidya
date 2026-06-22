ed6cf305ce26160c762a2602d3030c13ca4c180f
Shashi9648 <shashikumarradam26@gmail.com>
2026-06-22 14:57:30 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<StudentForm />} />
         <Route path="/student-form" element={<StudentForm />} />
         <Route path="/student-form/:referralCode" element={<StudentForm />} />
       </Routes>
